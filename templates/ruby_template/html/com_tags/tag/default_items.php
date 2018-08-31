cd5e5c19ad649ae785cdb80d4b8aff6477829225
Tran Cong <trancong.joomla@gmail.com>
2018-08-31 16:40:10 +0700

    Edit button search tag

--- templates/ruby_template/html/com_tags/tag/default_items.php
+++ templates/ruby_template/html/com_tags/tag/default_items.php
@@ -34,9 +34,6 @@ JFactory::getDocument()->addScriptDeclaration("
 		<fieldset class="filters btn-toolbar">
 			<?php if ($this->params->get('filter_field')) : ?>
 				<div class="btn-group">
-					<label class="filter-search-lbl element-invisible" for="filter-search">
-						<?php echo JText::_('COM_TAGS_TITLE_FILTER_LABEL') . '&#160;'; ?>
-					</label>
 					<input type="text" name="filter-search" id="filter-search" value="<?php echo $this->escape($this->state->get('list.filter')); ?>" class="inputbox" onchange="document.adminForm.submit();" title="<?php echo JText::_('COM_TAGS_FILTER_SEARCH_DESC'); ?>" placeholder="<?php echo JText::_('COM_TAGS_TITLE_FILTER_LABEL'); ?>" />
 					<button type="button" name="filter-search-button" title="<?php echo JText::_('JSEARCH_FILTER_SUBMIT'); ?>" onclick="document.adminForm.submit();" class="btn">
 						<span class="icon-search"></span>
